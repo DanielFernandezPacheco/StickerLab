ae73c4836a07f9976cc22297d7bfb0815580c137
Daniel Fernández-Pacheco Santamaría <55846085+DanielFernandezPacheco@users.noreply.github.com>
2021-04-24 01:14:18 +0200

    recibe sticker desde whatsapp (solo para testing)

--- app/src/main/java/es/fdi/stickerlab/MainActivity.java
+++ app/src/main/java/es/fdi/stickerlab/MainActivity.java
@@ -2,13 +2,58 @@ package es.fdi.stickerlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent requestFileIntent;
+    private ParcelFileDescriptor inputPFD;
+    private ImageView img;
+    private TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        img = (ImageView) findViewById(R.id.image);
+        info = (TextView) findViewById(R.id.info);
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/webp")) {// con image/webp solo recibe stickers
+                handleSendImage(intent); // Handle single image being sent
+            }
+        } else {
+            Log.d("RECIBIDO", "otro");
+        }
+
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            //img.setImageResource(R.drawable.intent.);
+            Log.d("RECIBIDO", "imagen");
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
+                img.setImageBitmap(bitmap);
+                info.setVisibility(View.GONE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
