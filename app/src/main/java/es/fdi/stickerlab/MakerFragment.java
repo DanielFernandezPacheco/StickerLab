27f423e2a5a784cf6dd5a2498a2df83b5119852d
luispecos <79973236+luispecos@users.noreply.github.com>
2021-05-17 22:14:57 +0200

    Update MakerFragment.java

--- app/src/main/java/es/fdi/stickerlab/MakerFragment.java
+++ app/src/main/java/es/fdi/stickerlab/MakerFragment.java
@@ -1,23 +1,83 @@
 package es.fdi.stickerlab;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
+
+
 public class MakerFragment  extends Fragment {
+
+ private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+    ImageView imagen;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_maker, container, false);
+
+
+    }
+
+    public void onclick(View view){
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        //intent.setType("image/");
+       // startActivityForResult(intent.createChooser(intent,"Selecciona la aplicación"),10);
+
+
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imagen.setImageURI(imageUri);
+
+
+            //Intent intent = getIntent();
+            //receiveStickerIntent(intent);
+
+        }
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        imagen = (ImageView)view.findViewById(R.id.imagenId);
+
+
     }
+
+
+
 }
