dfc997411b1ab60f48d5f8d86e6f5158fe1677b5
luispecos <79973236+luispecos@users.noreply.github.com>
2021-05-18 14:32:56 +0200

    Update MakerFragment.java
    Actualizar el maker

--- app/src/main/java/es/fdi/stickerlab/MakerFragment.java
+++ app/src/main/java/es/fdi/stickerlab/MakerFragment.java
@@ -1,6 +1,7 @@
 package es.fdi.stickerlab;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,15 +11,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
@@ -28,8 +34,15 @@ import static android.app.Activity.RESULT_OK;
 public class MakerFragment  extends Fragment {
 
  private static final int PICK_IMAGE = 100;
+    private final Context mContext;
     Uri imageUri;
     ImageView imagen;
+
+
+    public MakerFragment(Context context) {
+        super();
+        mContext = context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +55,7 @@ public class MakerFragment  extends Fragment {
 
     }
 
-    public void onclick(View view){
+    public void onClick(View view){
         cargarImagen();
     }
 
@@ -62,21 +75,40 @@ public class MakerFragment  extends Fragment {
         if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
             imageUri = data.getData();
             imagen.setImageURI(imageUri);
+            
 
+        }
+        if (imageUri != null) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-            //Intent intent = getIntent();
-            //receiveStickerIntent(intent);
+            // cambiar y coger directamente de la bbdd para que no de problemas de null pointer, que los da jaja
+            ArrayList<String> categories = CategoriesFragment.getCategories();
 
+            new ReceivedStickerDialog(mContext, bitmap, categories);
         }
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final Button butt = view.findViewById(R.id.btnCargarImg);
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarImagen();
+
+            }
+        });
         imagen = (ImageView)view.findViewById(R.id.imagenId);
 
 
     }
+
 
 
 
